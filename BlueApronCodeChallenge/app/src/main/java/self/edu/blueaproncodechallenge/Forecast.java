6513f55fa54469bba0695a9260ec9cace493633e
Dare Ryan <Dareryan@me.com>
2016-01-12 19:01:17 -0500

    Add accessor methods to Forecast class. Configure RecycleView View Holder to access instance variables on class

--- BlueApronCodeChallenge/app/src/main/java/self/edu/blueaproncodechallenge/Forecast.java
+++ BlueApronCodeChallenge/app/src/main/java/self/edu/blueaproncodechallenge/Forecast.java
@@ -1,5 +1,11 @@
 package self.edu.blueaproncodechallenge;
 
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.PriorityQueue;
 
@@ -8,13 +14,80 @@ import java.util.PriorityQueue;
  */
 public class Forecast {
 
-    private Date date;
-    private String weatherDescription;
-    private String weatherIconID;
-    private Double currentTemp;
-    private Double minimumTemp;
-    private Double maximumTemp;
+    private Date date = new Date();
+    private String weatherDescription = "";
+    private String  weatherIconID = "";
+    private Double currentTemp = 0.0;
+    private Double minimumTemp = 0.0;
+    private Double maximumTemp = 0.0;
 
 
+    public Drawable weatherIcon() {
+        return drawableForWeatherIconID(weatherIconID);
+    }
+
+    public String weatherDescription() {
+        return this.weatherDescription;
+    }
+
+    public  String headerText() {
+        String date = new SimpleDateFormat("MMMM dd").format(this.date);
+        return String.format("%s  %s", date, date);
+    }
+
+    public String currentTemp() {
+        return String.format("%.1f", this.currentTemp);
+    }
+
+    public String minTemp() {
+        return String.format("%.1f", this.minimumTemp);
+    }
+
+    public String maxTemp() {
+        return String.format("%.1f", this.maximumTemp);
+    }
+
+    private Drawable drawableForWeatherIconID(String id) {
+        switch (id) {
+            case "01d":
+                return Resources.getSystem().getDrawable(R.drawable.image_01d, null);
+            case "02d":
+                return Resources.getSystem().getDrawable(R.drawable.image_02d, null);
+            case "03d":
+                return Resources.getSystem().getDrawable(R.drawable.image_03d, null);
+            case "04d":
+                return Resources.getSystem().getDrawable(R.drawable.image_04d, null);
+            case "09d":
+                return Resources.getSystem().getDrawable(R.drawable.image_09d, null);
+            case "10d":
+                return Resources.getSystem().getDrawable(R.drawable.image_10d, null);
+            case "11d":
+                return Resources.getSystem().getDrawable(R.drawable.image_11d, null);
+            case "13d":
+                return Resources.getSystem().getDrawable(R.drawable.image_13d, null);
+            case "50d":
+                return Resources.getSystem().getDrawable(R.drawable.image_50d, null);
+            case "01n":
+                return Resources.getSystem().getDrawable(R.drawable.image_01n, null);
+            case "02n":
+                return Resources.getSystem().getDrawable(R.drawable.image_02n, null);
+            case "03n":
+                return Resources.getSystem().getDrawable(R.drawable.image_03n, null);
+            case "04n":
+                return Resources.getSystem().getDrawable(R.drawable.image_04n, null);
+            case "09n":
+                return Resources.getSystem().getDrawable(R.drawable.image_09n, null);
+            case "10n":
+                return Resources.getSystem().getDrawable(R.drawable.image_10n, null);
+            case "11n":
+                return Resources.getSystem().getDrawable(R.drawable.image_11n, null);
+            case "13n":
+                return Resources.getSystem().getDrawable(R.drawable.image_13n, null);
+            case "50n":
+                return Resources.getSystem().getDrawable(R.drawable.image_50n, null);
+            default:
+                return Resources.getSystem().getDrawable(R.drawable.image_01d, null);
+        }
+    }
 
 }
--- BlueApronCodeChallenge/app/src/main/java/self/edu/blueaproncodechallenge/ForecastAdaptor.java
+++ BlueApronCodeChallenge/app/src/main/java/self/edu/blueaproncodechallenge/ForecastAdaptor.java
@@ -1,21 +1,49 @@
 package self.edu.blueaproncodechallenge;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
+
 
 /**
  * Created by Dare on 1/12/16.
  */
 public class ForecastAdaptor extends RecyclerView.Adapter<ForecastAdaptor.ForecastViewHolder>{
+
+    private LayoutInflater inflater;
+    List<Forecast> data = Collections.emptyList();
+
+    public ForecastAdaptor(Context context, List<Forecast> data) {
+        inflater = LayoutInflater.from(context);
+        this.data = data;
+    }
+
     @Override
     public ForecastViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = inflater.inflate(R.layout.view_forecast_cell, parent, false);
+        ForecastViewHolder holder = new ForecastViewHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(ForecastViewHolder holder, int position) {
 
+        Forecast forecast = data.get(position);
+
+        holder.imageView.setImageDrawable(forecast.weatherIcon());
+        holder.forecastDescription.setText(forecast.weatherDescription());
+        holder.headerTextView.setText(forecast.headerText());
+        holder.currentTempTextView.setText(forecast.currentTemp());
+        holder.lowTempTextView.setText(forecast.minTemp());
+        holder.highTempTextView.setText(forecast.maxTemp());
     }
 
     @Override
@@ -25,8 +53,21 @@ public class ForecastAdaptor extends RecyclerView.Adapter<ForecastAdaptor.Foreca
 
     class ForecastViewHolder extends RecyclerView.ViewHolder{
 
+        ImageView imageView;
+        TextView forecastDescription;
+        TextView headerTextView;
+        TextView currentTempTextView;
+        TextView lowTempTextView;
+        TextView highTempTextView;
+
         public ForecastViewHolder(View itemView) {
             super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.forecastImageView);
+            forecastDescription = (TextView) itemView.findViewById(R.id.forecastDescriptionTextView);
+            headerTextView = (TextView) itemView.findViewById(R.id.titleTextView);
+            currentTempTextView = (TextView) itemView.findViewById(R.id.currentTempTextView);
+            highTempTextView = (TextView) itemView.findViewById(R.id.maxTempTextView);
+            lowTempTextView = (TextView) itemView.findViewById(R.id.minTempTextView);
         }
     }
 }
